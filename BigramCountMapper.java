0d5586028ffb0c56edef4194321638b4fe0cde6b
DavHer <dav93herdoiza93@gmail.com>
2016-12-08 09:18:25 -0600

    Create bla

---
+++ BigramCountMapper.java
@@ -0,0 +1,29 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class BigramCountMapper extends
+        Mapper<Object, Text, Bigrama, IntWritable> {
+
+    private final IntWritable ONE = new IntWritable(1);
+
+
+
+    public void map(Object key, Text value, Context context)
+            throws IOException, InterruptedException {
+
+        String[] tokens = value.toString().split("\\s+");
+        this.emitBigram("", tokens[0], context);
+        for (int i = 0; i < tokens.length - 1; i++) {
+            this.emitBigram(tokens[i], tokens[i + 1], context);
+        }
+        this.emitBigram(tokens[tokens.length - 1], "", context);
+    }
+
+    private void emitBigram(String a, String b, Context context) throws IOException, InterruptedException {
+        Bigrama bigram = new Bigrama(a, b);
+        context.write(bigram, ONE);
+    }
+}
